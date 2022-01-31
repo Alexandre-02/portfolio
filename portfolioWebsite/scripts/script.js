05deb154a67f1bd4d0d4c8700888afefcc786f53
Alexandre-02 <89979410+Alexandre-02@users.noreply.github.com>
2022-01-31 13:10:35 +0100

    Add files via upload

---
+++ portfolioWebsite/scripts/script.js
@@ -0,0 +1,24 @@
+//JavaScript Document
+
+// hamburger menu 
+var hamburgerMenu = document.querySelector("header nav div ul");
+
+var menuButton = document.querySelector(".hamburger-icon");
+
+var closeButton = document.querySelector(".icon-close");
+
+
+
+menuButton.addEventListener("click", addHamburgerMenu);
+
+function addHamburgerMenu(){
+
+    hamburgerMenu.classList.add("visible");
+
+}
+
+closeButton.addEventListener("click", closeHamburgerMenu);
+
+function closeHamburgerMenu(){
+    hamburgerMenu.classList.remove("visible");
+}
